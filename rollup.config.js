f0708825ca1d3e1dfd36b0ac70186bd204f8a417
RangHo Lee <hello@rangho.me>
2024-03-01 20:49:51 +0100

    style: apply Prettier

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,18 @@
-import commonjs from '@rollup/plugin-commonjs';
-import nodeResolve from '@rollup/plugin-node-resolve';
-import terser from '@rollup/plugin-terser';
-import typescript from '@rollup/plugin-typescript';
-import copy from 'rollup-plugin-copy';
+import commonjs from "@rollup/plugin-commonjs";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
+import typescript from "@rollup/plugin-typescript";
+import copy from "rollup-plugin-copy";
 
 export default {
-  input: 'src/index.ts',
+  input: "src/index.ts",
   output: {
-    dir: 'dist',
-    format: 'es',
+    dir: "dist",
+    format: "es",
   },
   plugins: [
     copy({
-      targets: [
-        { src: 'src/lisp', dest: 'dist' },
-      ]
+      targets: [{ src: "src/lisp", dest: "dist" }],
     }),
     nodeResolve(),
     commonjs(),
