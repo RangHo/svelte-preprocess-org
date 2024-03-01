a16f177ade6f12ade6c21854d886c90041324924
RangHo Lee <hello@rangho.me>
2024-03-01 20:50:16 +0100

    feat: generate optional cmdline arguments

--- src/emacs.ts
+++ src/emacs.ts
@@ -1,10 +1,30 @@
 import { execSync } from "node:child_process";
-import { join } from 'node:path';
+import { join } from "node:path";
 
-export interface OxSvelteOptions {}
+export interface OxSvelteOptions {
+  latexEnvironmentFormat?: string;
+  latexFragmentFormat?: string;
+  srcBlockFormat?: string;
+}
 
 export function convert(content: string, options?: OxSvelteOptions): string {
-  let command = `/usr/bin/env emacs --script ${join(import.meta.dirname, 'lisp', 'convert.el')}`;
+  const { latexEnvironmentFormat, latexFragmentFormat, srcBlockFormat } =
+    options || {};
+
+  let command = `/usr/bin/env emacs --script ${join(
+    import.meta.dirname,
+    "lisp",
+    "convert.el",
+  )}`;
+  if (latexEnvironmentFormat) {
+    command += ` --latex-environment-format ${latexEnvironmentFormat}`;
+  }
+  if (latexFragmentFormat) {
+    command += ` --latex-fragment-format ${latexFragmentFormat}`;
+  }
+  if (srcBlockFormat) {
+    command += ` --src-block-format ${srcBlockFormat}`;
+  }
 
   return execSync(command, { input: content }).toString();
 }
