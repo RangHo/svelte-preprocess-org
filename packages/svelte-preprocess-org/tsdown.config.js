9b03ce0ee65542aa7a21ae6047e4f9a3be185067
RangHo Lee <hello@rangho.me>
2025-08-06 15:44:50 +0900

    build: make ox-svelte a devDependency and copy the file

--- packages/svelte-preprocess-org/tsdown.config.js
+++ packages/svelte-preprocess-org/tsdown.config.js
@@ -1,6 +1,21 @@
 import { defineConfig } from "tsdown/config";
 
+import fs from "node:fs";
+import path from "node:path";
+
+function copyFile(from, to) {
+  return {
+    name: "copy-file",
+    generateBundle() {
+      fs.copyFileSync(path.resolve(from), path.resolve(to));
+    },
+  };
+}
+
 /** @type { import('tsdown/config').UserConfig } */
 export default defineConfig({
   sourcemap: process.env.NODE_ENV !== "production",
+  plugins: [
+    copyFile("./node_modules/ox-svelte/ox-svelte.el", "./dist/ox-svelte.el"),
+  ],
 });
